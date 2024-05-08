bb80657cb6bc4c292fde8b8dcbbf2b659754534c
vianie-devops <nodemtiayo@gmail.com>
2024-05-07 19:36:13 -0500

    code

---
+++ backend.tf
@@ -0,0 +1,10 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "week10-sk-terraform"
+    key            = "week10/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "state-log"
+  }
+}
