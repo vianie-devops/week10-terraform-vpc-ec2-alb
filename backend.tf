80da236ffa54cec8b596d4da9d3b1c09f48c8470
vianie-devops <nodemtiayo@gmail.com>
2024-05-07 20:22:33 -0500

    code

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "week10-sk-terraform-1"
+    bucket         = "week10-sk-terraform"
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
