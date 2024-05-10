b924bb4b46967df917ad915908949b810da5df21
vianie-devops <nodemtiayo@gmail.com>
2024-05-09 20:13:08 -0500

    new files modified

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "server1" {
   ami = "ami-02d7fd1c2af6eead0"
-  instance_type = "t2.micro"
+  instance_type = var.INSTANCE_TYPE
   vpc_security_group_ids = [ aws_security_group.sg1.id ]
-  availability_zone = "us-east-1a"
+  availability_zone = "${var.REGION}a"
   subnet_id = aws_subnet.private1.id
   user_data = file("code.sh")
   tags={
-    Name = "webserver-1"
+    Name = var.ENVIRONMENT
   }
 
 }
@@ -19,7 +19,7 @@ resource "aws_instance" "server2" {
   user_data = file("code.sh")
   tags={
     Name = "webserver-2"
-  }
+  } 
 
 }
 
