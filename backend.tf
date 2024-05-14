c398937f5fa6101d19d292a502286074ae46d66a
vianie-devops <nodemtiayo@gmail.com>
2024-05-13 21:36:17 -0500

    modified

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "state-log"
   }
 }
